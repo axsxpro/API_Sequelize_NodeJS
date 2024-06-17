ba51b49b9b9ef7005abc9502ce0a2c6390aa2d63
axsxpro <cmptpro@outlook.com>
2024-06-17 16:51:11 +0200

    maj swagger JWT

--- swagger/swaggerConfig.js
+++ swagger/swaggerConfig.js
@@ -1,17 +1,32 @@
-const swaggerJSDoc = require('swagger-jsdoc');
+const swaggerJSDoc = require("swagger-jsdoc");
 
 const options = {
-  definition: {
-    openapi: '3.0.0',
-    info: {
-      title: 'Beer',
-      version: '1.0.0',
-      description: 'API documentation for Node.js application',
+    definition: {
+        openapi: "3.0.0", // Version de l'OpenAPI
+        info: {
+            title: "Beer", // Titre de la documentation API
+            version: "1.0.0", // Version de la documentation API
+            description: "API documentation for Node.js application", // Description de la documentation API
+        },
+        components: {
+            securitySchemes: {
+                JwtAuth: { 
+                    type: "http",
+                    scheme: "bearer",
+                    bearerFormat: "JWT",
+                },
+            },
+        },
+        security: [
+            {
+                JwtAuth: [],
+            },
+        ],
     },
-  },
-  apis: ['./src/Routes/*.js'], // Chemin vers les fichiers de routes contenant toutes les requetes HTTP
+    apis: ["./src/Routes/*.js"], // Chemin vers les fichiers de routes contenant toutes les requêtes HTTP
 };
 
 const swaggerSpec = swaggerJSDoc(options);
 
 module.exports = swaggerSpec;
+
